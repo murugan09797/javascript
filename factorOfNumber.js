a6921c037096b65d1de8462e742f340c802334e9
selva murugan <selvamurugan.rathinam@colanonline.com>
2022-04-06 13:52:51 +0530

    Foctors of Number

--- factorOfNumber.js
+++ factorOfNumber.js
@@ -1,2 +1,17 @@
-console.log("hello world")
-console.log("hao")
+
+// Program to Find the Factorial Number
+
+const value = 12
+
+const output = []
+function factorial (arg){
+ 
+for (let i = 1; i<= value; i++){
+    if(value % i == 0)
+      output.push(i)
+}
+
+    return output
+
+}
+console.log("factorial",factorial(value))
